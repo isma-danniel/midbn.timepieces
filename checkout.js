b91121de9e00cdc48b9f898daf3346ffe33badec
isma-danniel <isma.danniel1211@gmail.com>
2026-02-12 16:10:10 +0800

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -7,7 +7,7 @@
 // ==========================================
 
 const API =
-  "https://script.google.com/macros/s/AKfycbxPBR09YFOCTy_YTWn-gCybE8B5awe09zDS7ArXKpWthULDerPpqDGzunViAoPKH3nr/exec";
+  "https://script.google.com/macros/s/AKfycbzWf_6LH3bahkpuxMGVizwU--mwRBKqEm32bubQ1DhLdn3ugHmuK-9yToOZ_0i5pTe9ew/exec";
 
 const cartItemsContainer = document.getElementById("cartItems");
 const cartTotalEl = document.getElementById("cartTotal");
