f80f0b88c0fc7073a2816aba20cd6c19bd704b2f
isma-danniel <isma.danniel1211@gmail.com>
2026-02-15 20:38:10 +0800

    Update script-products.js

--- script-products.js
+++ script-products.js
@@ -11,7 +11,7 @@
 // ==========================================
 
 const API =
-  "https://script.google.com/macros/s/AKfycbx5FX_Id6eW6DPPVrGK4IMAJGJ1sxBcVWsfqdv3wYs-cZXNuUfm8zCAd72b20b6tpZu/exec";
+  "https://script.google.com/macros/s/AKfycbzRr3ySWQ_RbEufl1zlG2thCio_qRnTG9f6Kkvmdb1Z8aJ7JnHaslspD4NaRHcTIgXZ/exec";
 
 // ---------- Helpers ----------
 function normId(v){ return String(v ?? "").trim(); }
